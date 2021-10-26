49bac81fe93105fc2660dbd8f8004a38db8aa9d0
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-26 13:39:21 -0300

    added showcase component

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,13 @@
-import Head from "next/head"
+import Head from "next/head";
+import {useRouter} from "next/router";
 import Header from "./Header";
 import styles from "@/styles/Layout.module.css"
 import Footer from "./Footer";
+import Showcase from "@/components/Showcase";
 
 
 export default function Layout({title,keywords,description,children}){
+    const router = useRouter();
     return(
         <div>
             <Head>
@@ -14,6 +17,8 @@ export default function Layout({title,keywords,description,children}){
             </Head>
 
             <Header/>
+
+            {router.pathname === "/" ? <Showcase/> : ""}
 
             <div className={styles.container}>
                 {children}
---
+++ components/Showcase.js
@@ -0,0 +1,11 @@
+import styles from "@/styles/Showcase.module.css";
+
+
+export default function Showcase(){
+    return(
+        <div className={styles.showcase}>
+            <h1>Welcome to the party!</h1>
+            <h2>Find the best music events in Mendoza</h2>
+        </div>
+    )
+}

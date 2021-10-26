e2364fd39e345790fcc58dfd07d27ee69772ba85
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-26 16:13:13 -0300

    basic display of events, home page and event page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,29 @@
-import Layout from "../../components/Layout";
+import Layout from "@/components/Layout";
+import {API_URL} from "@/config/index";
+import EventItem from "@/components/EventItem";
 
-export default function EventsPage(){
-    return(
+export default function EventsPage({events}) {
+    return (
         <Layout>
-            <h1>My Events</h1>
+            <h1>Events</h1>
+            { events.length === 0 && <h3>No events to show</h3>}
+
+            {events.map(evt => (
+                <EventItem key={evt.id} evt={evt} />
+            ))}
         </Layout>
     )
+}
+
+export async function getStaticProps(){
+    const res = await fetch(`${API_URL}/api/events`)
+    let events = await res.json();
+
+    events = events["events"]
+
+    return{
+        props: {
+            events
+        }
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,38 @@
 import Layout from "@/components/Layout";
 import {API_URL} from "@/config/index";
+import EventItem from "@/components/EventItem";
+import Link from "next/link";
 
 export default function Home({events}) {
-    console.log(events)
   return (
       <Layout>
           <h1>Upcoming Events</h1>
+          { events.length === 0 && <h3>No events to show</h3>}
+
+          {events.map(evt => (
+              <EventItem key={evt.id} evt={evt} />
+          ))}
+
+          {events.length > 0 && (
+              <Link href="/events">
+                  <a className="btn-secondary">View all events</a>
+              </Link>
+          )}
       </Layout>
   )
 }
 
 export async function getStaticProps(){
     const res = await fetch(`${API_URL}/api/events`)
-    const events = res.json();
+    let events = await res.json();
+
+    events = events["events"]
 
     console.log(events)
 
     return{
         props: {
-            events
-        },
-        revalidate: 1
+            events: events.slice(0,3)
+        }
     }
 }

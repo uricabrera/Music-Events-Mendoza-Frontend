3231b055b4a5f82d7baeace3053c294b84acf773
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-27 15:38:24 -0300

    added react-toastify, TODO: create a slug in next.js with slugify

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from "@/styles/Header.module.css";
+import Search from "@/components/Search";
 
 export default function Header(){
     return(
@@ -9,11 +10,20 @@ export default function Header(){
                     <a>Music Events Mendoza</a>
                 </Link>
             </div>
+
+            <Search/>
+
+
             <nav>
                 <ul>
                     <li>
                         <Link href="/events">
                             <a>Events</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/events/add">
+                            <a>Add Event</a>
                         </Link>
                     </li>
                 </ul>

ef685d8cf3c2baf90005a7f22a46791e4c5555a6
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-27 17:24:02 -0300

    added pagination to the events/index.js

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,14 @@
 import Layout from "@/components/Layout";
 import {API_URL} from "@/config/index";
 import EventItem from "@/components/EventItem";
+import Link from "next/link";
 
-export default function EventsPage({events}) {
+const PER_PAGE = 2;
+
+export default function EventsPage({events,page,total}) {
+    const lastPage = Math.ceil(total / PER_PAGE)
+
+
     return (
         <Layout>
             <h1>Events</h1>
@@ -11,18 +17,45 @@ export default function EventsPage({events}) {
             {events.map(evt => (
                 <EventItem key={evt.id} evt={evt} />
             ))}
+
+            {page > 1 && (
+                <Link href={`/events?page=${page-1}`}>
+                    <a className="btn-secondary">Prev</a>
+                </Link>
+            )}
+
+            {page < lastPage && (
+                <Link href={`/events?page=${page+1}`}>
+                    <a className="btn-secondary">Next</a>
+                </Link>
+            )}
         </Layout>
     )
 }
 
-export async function getStaticProps(){
-    const res = await fetch(`${API_URL}/events?_sort=date:ASC`)
+export async function getServerSideProps({query:{page=1}}){
+
+    // Calculate start page
+
+    const start = +page === 1 ? 0 : (+page -1) * PER_PAGE
+
+    // Fetch total/count
+
+    const totalRes = await fetch(`${API_URL}/events/count`)
+
+    const total = await totalRes.json();
+
+
+    // Fetch events
+
+    const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`)
     let events = await res.json();
 
     return{
         props: {
-            events
-        },
-        revalidate: 1
+            events,
+            page: +page,
+            total
+        }
     }
 }
